32cbfeb655276b8648071acede53c5162ebf2f8b
littlewing <mail@alexandra-jaeger.net>
2026-01-28 19:11:07 +0100

    logging middleware added

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+const morgan = require("morgan");
+const { createWriteStream } = require("fs");
+
 app.use(express.static("public"));
+
+app.use(
+    morgan("tiny", { stream: createWriteStream("./app.log", { flags: "a" }) }),
+);
 
 const jellybeanBag = {
     mystery: {
@@ -21,16 +28,23 @@ const jellybeanBag = {
     },
 };
 
-// Logging Middleware
-app.use((req, res, next) => {
-    console.log(`${req.method} Request Received`);
-    next();
-});
+const bodyParser = (req, res, next) => {
+    let queryData = "";
+    req.on("data", (data) => {
+        data = data.toString();
+        queryData += data;
+    });
+    req.on("end", () => {
+        if (queryData) {
+            req.body = JSON.parse(queryData);
+        }
+        next();
+    });
+};
 
 app.use("/beans/:beanName", (req, res, next) => {
     const beanName = req.params.beanName;
     if (!jellybeanBag[beanName]) {
-        console.log("Response Sent");
         return res.status(404).send("Bean with that name does not exist");
     }
     req.bean = jellybeanBag[beanName];
@@ -38,79 +52,54 @@ app.use("/beans/:beanName", (req, res, next) => {
     next();
 });
 
-app.use(["/beans", "/beans/:beanName"], (req, res, next) => {
-    let bodyData = "";
-    req.on("data", (data) => {
-        bodyData += data;
-    });
-    req.on("end", () => {
-        if (bodyData) {
-            req.body = JSON.parse(bodyData);
-        }
-        next();
-    });
-});
-
 app.get("/beans", (req, res, next) => {
     res.send(jellybeanBag);
-    console.log("Response Sent");
 });
 
-app.post("/beans", (req, res, next) => {
-    const beanName = req.body.name;
+app.post("/beans", bodyParser, (req, res, next) => {
+    const body = req.body;
+    const beanName = body.name;
     if (jellybeanBag[beanName] || jellybeanBag[beanName] === 0) {
         return res.status(400).send("Bean with that name already exists!");
     }
-    const numberOfBeans = Number(req.body.number) || 0;
+    const numberOfBeans = Number(body.number) || 0;
     jellybeanBag[beanName] = {
         number: numberOfBeans,
     };
     res.send(jellybeanBag[beanName]);
-    console.log("Response Sent");
 });
 
 app.get("/beans/:beanName", (req, res, next) => {
     res.send(req.bean);
-    console.log("Response Sent");
 });
 
-app.post("/beans/:beanName/add", (req, res, next) => {
+app.post("/beans/:beanName/add", bodyParser, (req, res, next) => {
     const numberOfBeans = Number(req.body.number) || 0;
     req.bean.number += numberOfBeans;
     res.send(req.bean);
-    console.log("Response Sent");
 });
 
-app.post("/beans/:beanName/remove", (req, res, next) => {
+app.post("/beans/:beanName/remove", bodyParser, (req, res, next) => {
     const numberOfBeans = Number(req.body.number) || 0;
     if (req.bean.number < numberOfBeans) {
         return res.status(400).send("Not enough beans in the jar to remove!");
     }
     req.bean.number -= numberOfBeans;
     res.send(req.bean);
-    console.log("Response Sent");
 });
 
 app.delete("/beans/:beanName", (req, res, next) => {
-    const beanName = req.params.beanName;
-    if (!req.bean) {
-        return res.status(404).send("Bean with that name does not exist");
-    }
-    req.bean = null;
+    const beanName = req.beanName;
+    jellybeanBag[beanName] = null;
     res.status(204).send();
-    console.log("Response Sent");
 });
 
-app.put("/beans/:beanName/name", (req, res, next) => {
-    const beanName = req.params.beanName;
-    if (!req.bean) {
-        return res.status(404).send("Bag with that name does not exist");
-    }
+app.put("/beans/:beanName/name", bodyParser, (req, res, next) => {
+    const beanName = req.beanName;
     const newName = req.body.name;
     jellybeanBag[newName] = req.bean;
-    req.bean = null;
+    jellybeanBag[beanName] = null;
     res.send(jellybeanBag[newName]);
-    console.log("Response Sent");
 });
 
 // export app for use in main.js and for testing
